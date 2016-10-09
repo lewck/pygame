dd6d958684db79d9c6924391207aaec333b2cbed
sleawnis <sleawnis@googlemail.com>
2016-10-09 22:14:51 +0100

    changed ui to new merged

---
+++ legacy/ui/uis/base.py
@@ -0,0 +1,29 @@
+from engine.event import event
+from legacy.ui.uis.ioobject import ioobject
+
+
+class base():
+    def __init__(self, **kwargs):
+        self.activeOutput = []
+        self.activeInput = []
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def addOutput(self, **kwargs):
+        self.activeOutput.append(ioobject(**kwargs, parent=self.getSubClassName()))
+
+    def addOutputAttrs(self, args):
+        self.activeOutput[len(self.activeOutput)-1].addAttrs(args)
+
+    def addInput(self, **kwargs):
+        self.activeInput.append(ioobject(**kwargs, parent=self.getSubClassName()))
+
+    def addInputAttrs(self, args):
+        self.activeInput[len(self.activeInput)-1].addAttrs(args)
+
+    def addEvent(self, name):
+        event.create(self)
+
+    def doEve(self):
+        print('hi')
