b811b9460a62eec7f0e3c75e48e078753876ce4d
sleawnis <sleawnis@googlemail.com>
2016-10-14 23:14:45 +0100

    todo fix farm making 2 jobs a pop

--- object/farm_1.py
+++ object/farm_1.py
@@ -11,7 +11,7 @@ class farm_1(base):
 
         super(farm_1, self).setVars(image='farm_1', **kwargs)
         self.passable = []
-        self.inventory = inventory(30)
+        self.inventory = inventory(500)
         self.status = 0
 
 
@@ -21,8 +21,7 @@ class farm_1(base):
     def doTick(self, tickID):
         if(tickID==0):
             #chance grow increase
-            print('cha')
-            if(self.inventory.isFull()) & (self.status == 1):
+            if(self.inventory.isFull()):
                 self.image = self.load(self.title)
 
 
@@ -30,5 +29,7 @@ class farm_1(base):
             if(self.inventory.addItem('vegetableCarrot',8)=='INVFULL') & (self.status!=1):
 
                 self.image = self.load(self.title+'_full')
+
+                print('ADDING JOB')
                 if(jobset.create(typ='collectFromObjectAndStore', startPosition=[self.y, self.x, self.direction])):
                     self.status = 1
