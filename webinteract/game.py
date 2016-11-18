bba454880693231f1f554a757fc8459c948fc422
sleawnis <sleawnis@googlemail.com>
2016-11-18 12:44:09 +0000

    Working on adding objectives

--- webinteract/game.py
+++ webinteract/game.py
@@ -8,7 +8,7 @@ class game(base):
         super(game, self).__init__()
 
     def create(self):
-        request = self.requestCall('creategame')
+        request = self.requestCall('creategame', {'goal_id': 1, 'goal_args': 50000})
 
         settings.gameData['session_id'] = request['session_id']
         settings.gameData['game_id'] = request['game_id']
